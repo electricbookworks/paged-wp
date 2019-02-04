7b640027e84375c0135b5ffbdb47f28134a94c62
Jonathan Bossenger <jonathanbossenger@gmail.com>
2019-02-04 14:40:53 +0200

    Initial set up
    Adds paged-wp.php plugin file
    Adds plugin scaffolding, sample tests, distignore file

---
+++ paged-wp.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Paged WP
+ * Plugin URI:  https://example.com/plugins/the-basics/
+ * Description: Work in progress, a WordPress plugin for using paged.js
+ * Version:     0.0.1
+ * Author:      Jonathan Bossenger
+ * Author URI:  https://jonathanbossenger
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: paged-wp
+ * Domain Path: /languages
+ */
